2bb200bc69715bb9414525b4d0c235a231eb9342
Marco Santos <marco@sociap.io>
2025-11-15 22:58:54 -0300

    success

--- js/cardano-connect.js
+++ js/cardano-connect.js
@@ -127,7 +127,6 @@ async function connectWallet() {
 
     if (!isNullOrEmptyOrUndefined(carteira)){
         user = await getPools(carteira);
-        console.log(user);
         if (!isNullOrEmptyOrUndefined(user.small)){
             btn = document.getElementById("btn-connect")
             btn.text = user.small + ' (' + user.pool + ' POOL)'
